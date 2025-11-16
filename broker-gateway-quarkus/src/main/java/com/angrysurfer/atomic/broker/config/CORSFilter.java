79fc8586d0ee8650a1cec20aa91d551e35860acd
markpippins <mpippins@gmail.com>
2025-11-16 12:30:11 -0500

    updated api, CORS

--- broker-gateway-quarkus/src/main/java/com/angrysurfer/atomic/broker/config/CORSFilter.java
+++ broker-gateway-quarkus/src/main/java/com/angrysurfer/atomic/broker/config/CORSFilter.java
@@ -11,10 +11,21 @@ public class CORSFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-requested-with, X-Custom-Header");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        responseContext.getHeaders().add("Access-Control-Expose-Headers", "*");
+        // Only set headers if they're not already set to avoid duplication
+        if (responseContext.getHeaders().get("Access-Control-Allow-Origin") == null) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        }
+        if (responseContext.getHeaders().get("Access-Control-Allow-Credentials") == null) {
+            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        }
+        if (responseContext.getHeaders().get("Access-Control-Allow-Headers") == null) {
+            responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-requested-with, X-Custom-Header, Content-Type");
+        }
+        if (responseContext.getHeaders().get("Access-Control-Allow-Methods") == null) {
+            responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        }
+        if (responseContext.getHeaders().get("Access-Control-Expose-Headers") == null) {
+            responseContext.getHeaders().add("Access-Control-Expose-Headers", "Content-Disposition, Content-Type, Location");
+        }
     }
 }
